c78a7f81588356d1af50605411ce95166a934627
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-04 23:32:47 -0500

    navbar and homepage with blogs displaying

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const Home = () => {  
+    const [blogs, setBlogs] = useState([
+        {title: 'My first blog!', body: 'Welcome!', author: 'Shadi', id: 1},
+        {title: 'My second blog!', body: 'Welcome again!', author: 'Shadi I', id: 2}
+    ]);
+    return ( 
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title} </h2>
+                    <p> Written by: {blog.author}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default Home;
