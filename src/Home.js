9c0c0eadeb225cb2d2225af08320ae8c18bc5685
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-08-05 19:10:35 -0500

    used filter method to diplay blogs from certain authors

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,12 @@ import BlogList from "./BlogList";
 const Home = () => {  
     const [blogs, setBlogs] = useState([
         {title: 'My first blog!', body: 'Welcome!', author: 'Shadi', id: 1},
-        {title: 'My second blog!', body: 'Welcome again!', author: 'Shadi I', id: 2}
+        {title: 'Great to be apart of this Community!', body: 'Oi!', author: 'Anon', id: 2}
     ]);
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs!" />
+           <BlogList blogs={blogs.filter((blog) => blog.author === "Shadi" )} title="Shadi's blogs" />
         </div>
      );
 
